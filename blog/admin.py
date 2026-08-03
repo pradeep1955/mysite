73bc1e2678ead643eb2bf7bdef8a60408fe6d42a
pradeep1955 <prdpchowdhary@gmail.com>
2026-08-03 20:52:57 +0530

    blog admin.py edited

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,15 @@
+#admin.site.register(Post)
+
 from django.contrib import admin
+from .models import Post, Category
 
-from .models import Post
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date_posted", "is_hidden")
+    list_filter = ("categories", "is_hidden")
+    filter_horizontal = ("categories",)
